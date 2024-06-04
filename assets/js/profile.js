1ea94823dc4ceac5c4a38a3adf5ac64226eae0d1
Abhishek kumar <abhishekkumar200218@gmail.com>
2024-06-04 11:22:44 +0530

    Upload page added to profile

---
+++ assets/js/profile.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const popupOverlay = document.getElementById('popupOverlay');
+
+    const popup = document.getElementById('popup');
+
+    const closePopup = document.getElementById('closePopup');
+
+
+    // Function to open the popup
+
+    function openPopup() {
+
+        popupOverlay.style.display = 'block';
+
+    }
+
+    // Function to close the popup
+
+    function closePopupFunc() {
+
+        popupOverlay.style.display = 'none';
+
+    }
+
+    // Event listeners
+
+    // Trigger the popup to open (you can call this function on a button click or any other event)
+    var button = document.getElementById("myButton");
+    button.addEventListener("click", openPopup);
+
+    // Close the popup when the close button is clicked
+
+    closePopup.addEventListener('click', closePopupFunc);
+
+    // Close the popup when clicking outside the popup content
+
+    popupOverlay.addEventListener('click', function (event) {
+
+        if (event.target === popupOverlay) {
+
+            closePopupFunc();
+
+        }
+
+    });
+});
